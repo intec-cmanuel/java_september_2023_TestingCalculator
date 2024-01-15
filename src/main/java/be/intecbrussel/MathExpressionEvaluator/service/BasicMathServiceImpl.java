2bd371054bef7060ec96dcb36e1c8013eb1b9a8d
Izuna <manuel.cogneau@intecbrussel.be>
2024-01-15 11:56:34 +0100

    Big Decimals for the win

--- src/main/java/be/intecbrussel/MathExpressionEvaluator/service/BasicMathServiceImpl.java
+++ src/main/java/be/intecbrussel/MathExpressionEvaluator/service/BasicMathServiceImpl.java
@@ -1,9 +1,19 @@
 package be.intecbrussel.MathExpressionEvaluator.service;
 
+import java.math.BigDecimal;
+
 public class BasicMathServiceImpl implements BasicMathService{
     @Override
     public double add(double firstNumber, double secondNumber) {
-        return firstNumber + secondNumber;
+        String firstNumberAsString = String.valueOf(firstNumber);
+        String secondNumberAsString = String.valueOf(secondNumber);
+
+        BigDecimal firstDecimal = new BigDecimal(firstNumberAsString);
+        BigDecimal secondDecimal = new BigDecimal(secondNumberAsString);
+
+        BigDecimal result = firstDecimal.add(secondDecimal);
+
+        return result.doubleValue();
     }
 
     @Override
--- src/test/java/be/intecbrussel/MathExpressionEvaluator/service/BasicMathServiceTest.java
+++ src/test/java/be/intecbrussel/MathExpressionEvaluator/service/BasicMathServiceTest.java
@@ -1,15 +1,18 @@
 package be.intecbrussel.MathExpressionEvaluator.service;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+import java.util.stream.Stream;
+
 public class BasicMathServiceTest {
-    private BasicMathService basicMathService;
+    private final BasicMathService basicMathService;
 
-    @BeforeAll
-    public void beforeAll() {
-        // TODO instantiate the service
-        basicMathService = new BasicMathServiceImpl();
+    {
+        this.basicMathService = new BasicMathServiceImpl();
     }
 
     @Test
@@ -22,5 +25,39 @@ public class BasicMathServiceTest {
         double result = basicMathService.add(firstNumber, secondNumber);
 
         Assertions.assertEquals(expectedValue, result);
+    }
+
+    @Test
+    public void testBasicAdditionOfTwoNegativeIntegers() {
+        int firstNumber = -4;
+        int secondNumber= -8;
+        int expectedResult = -12;
+
+        double result = basicMathService.add(firstNumber, secondNumber);
+
+        Assertions.assertEquals(expectedResult, result);
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("basicAdditionFactory")
+    public void testBasicAdditions(double number1, double number2, double expectedValue) {
+        double result = basicMathService.add(number1, number2);
+        Assertions.assertEquals(expectedValue, result);
+    }
+
+    public static Stream<Arguments> basicAdditionFactory() {
+        return Stream.of(
+                Arguments.of(5, 3, 8),
+                Arguments.of(50, 3, 53),
+                Arguments.of(-5, 3, -2),
+                Arguments.of(0, 0, 0),
+                Arguments.of(-7, -3, -10),
+                Arguments.of(5, -3, 2),
+                Arguments.of(5.5, 4.5, 10),
+                Arguments.of(2000000000, 2000000000, 4000000000L),
+                Arguments.of(-0.00001, 0.00002, 0.00001),
+                Arguments.of(0.99999, 0.000001, 0.999991)
+        );
     }
 }
